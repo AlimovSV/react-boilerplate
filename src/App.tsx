66fb0c233cecd20cba81117e268d1a27e6db3e65
Sergey Alimov <sergey.alimov@gmail.com>
2020-02-19 11:12:03 +0400

    Add react-intl

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { FormattedMessage, FormattedDate, FormattedNumber, defineMessages } from 'react-intl';
 import logo from './logo.svg';
 import './App.css';
+import I18nProvider from './i18n';
 
-function App() {
-  return (
+const App = () => (
+  <I18nProvider>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <FormattedMessage {...messages.text} values={{ link: <code>src/App.tsx</code> }} />
+          <br />
+          <FormattedDate value={Date.now()} format="long" />
+          <br />
+          <FormattedNumber value={123} format="USD" />
         </p>
         <a
           className="App-link"
@@ -16,11 +22,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <FormattedMessage {...messages.link} />
         </a>
       </header>
     </div>
-  );
-}
+  </I18nProvider>
+);
+
+const messages = defineMessages({
+  text: {
+    id: 'App/text',
+    defaultMessage: 'Edit {link} and save to reload.',
+  },
+  link: {
+    id: 'App/link',
+    defaultMessage: 'Learn React',
+  },
+});
 
 export default App;
---
+++ src/i18n/index.tsx
@@ -0,0 +1,44 @@
+// @flow
+
+import React from 'react';
+import { IntlProvider, IntlConfig } from 'react-intl';
+
+const formats = {
+  number: {
+    percent: {
+      style: 'percent',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    },
+    USD: {
+      style: 'currency',
+      currency: 'USD',
+    },
+  },
+  date: {
+    short: {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    },
+    long: {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    },
+  },
+};
+
+const messages = {};
+
+const I18nProvider: React.FC<Partial<IntlConfig>> = props => (
+  <IntlProvider
+    {...props}
+    locale="en"
+    defaultFormats={formats}
+    formats={formats}
+    messages={messages}
+  />
+);
+
+export default I18nProvider;
